5ed75b63f9e55710085f1189c5ac1b8ab56ab3a0
FrikBriers <frik.briers@novadoc.nl>
2024-05-24 12:47:40 +0200

    before lunch

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,13 @@
+import React from "react";
+import  {AppContext} from "../context/AppContext";
 
+const AllocationForm = (props) => {
+    const {dispatch, remaining} = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent
+
+};
